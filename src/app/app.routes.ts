3656b053fbaf38204e7cd1564388b833d8f5f080
Jodyannre <2098807910101@ingenieria.usac.edu.gt>
2024-11-23 23:35:46 -0600

    add project structure

---
+++ src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { Routes } from '@angular/router';
+import { AboutUsComponent } from '@pages/about-us/about-us.component';
+import { ContactUsComponent } from '@pages/contact-us/contact-us.component';
+import { HomeComponent } from '@pages/home/home.component';
+import { SingleCategoryComponent } from '@pages/single-category/single-category.component';
+import { TermsAndConditionComponent } from '@pages/terms-and-condition/terms-and-condition.component';
+
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'category', component: SingleCategoryComponent},
+    { path: 'post', component: SingleCategoryComponent},
+
+    { path: 'about', component: AboutUsComponent },
+    { path: 'term-condition', component: TermsAndConditionComponent },
+    { path: 'contact', component: ContactUsComponent },
+];
