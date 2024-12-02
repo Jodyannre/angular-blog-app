a07e9bb7983fca64210dbec1b412f5b921f6fee5
Jodyannre <2098807910101@ingenieria.usac.edu.gt>
2024-12-01 22:21:08 -0600

    add comments, subscription and single pages components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,13 @@ import { FooterComponent } from '@layouts/footer/footer.component';
 import { HeaderComponent } from '@layouts/header/header.component';
 import { PostCardComponent } from '@layouts/post-card/post-card.component';
 import { SubscriptionFormComponent } from './subscription-form/subscription-form.component';
+import { SinglePostComponent } from '@pages/single-post/single-post.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, CategoryNavbarComponent, FooterComponent, PostCardComponent, SubscriptionFormComponent],
+  imports: [RouterOutlet, HeaderComponent, CategoryNavbarComponent, 
+    FooterComponent, PostCardComponent, SubscriptionFormComponent, SinglePostComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,13 @@ import { AboutUsComponent } from '@pages/about-us/about-us.component';
 import { ContactUsComponent } from '@pages/contact-us/contact-us.component';
 import { HomeComponent } from '@pages/home/home.component';
 import { SingleCategoryComponent } from '@pages/single-category/single-category.component';
+import { SinglePostComponent } from '@pages/single-post/single-post.component';
 import { TermsAndConditionComponent } from '@pages/terms-and-condition/terms-and-condition.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'category', component: SingleCategoryComponent},
-    { path: 'post', component: SingleCategoryComponent},
+    { path: 'post', component: SinglePostComponent},
 
     { path: 'about', component: AboutUsComponent },
     { path: 'term-condition', component: TermsAndConditionComponent },
